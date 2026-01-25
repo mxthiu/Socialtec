d52e4f59d8ba1c97955a7bee8262d83729ecfecc
JJARRUS <picadoj944@gmail.com>
2026-01-25 14:56:59 -0600

    se implementó el esqueleto del proyecto

---
+++ servidor/gui_servidor.py
@@ -0,0 +1,8 @@
+"""
+GUI del servidor (ej. PyQt6).
+
+TODO:
+- Botones para iniciar/detener servidor.
+- Controles para solicitar camino, ver estadisticas y grafo.
+- Mostrar logs y estado de clientes conectados.
+"""
---
+++ utils/protocolo.py
@@ -0,0 +1,8 @@
+"""
+Codificacion y decodificacion de mensajes JSON.
+
+TODO:
+- Definir formato comun: type/action/payload/status/error.
+- Serializar/deserializar entre cliente y servidor.
+- Validar campos requeridos y codificacion segura.
+"""
